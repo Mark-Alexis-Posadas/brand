9152f2f1f98b4192529c86982030721d353222fc
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-09-20 17:33:39 +0800

    added project filter\

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
 import { PageTitle, StyledWrapper } from "../../globalStyles";
 import data from "./data";
-import { StyledArticle } from "./Projects.style";
+import { StyledArticle, StyledProjectGrid } from "./Projects.style";
 const Projects = () => {
   return (
     <section>
       <StyledWrapper>
         <PageTitle>Projects</PageTitle>
-        {data.map((projects) => {
-          const { id, image, pageTitle, description, source, demo } = projects;
-          return (
-            <StyledArticle key={id}>
-              <figure>
-                <img src={image} alt={image} />
-              </figure>
-              <header>
-                <h1>{pageTitle}</h1>
-              </header>
-              <p>{description}</p>
-            </StyledArticle>
-          );
-        })}
+        <div>
+          <div>
+            <button>Javascript</button>
+            <button>React JS</button>
+          </div>
+          <StyledProjectGrid>
+            {data.map((projects) => {
+              const { id, image, pageTitle, description, source, demo } =
+                projects;
+              return (
+                <StyledArticle key={id}>
+                  <figure>
+                    <img src={image} alt={image} />
+                  </figure>
+                  <header>
+                    <h1>{pageTitle}</h1>
+                  </header>
+                  <p>{description}</p>
+                </StyledArticle>
+              );
+            })}
+          </StyledProjectGrid>
+        </div>
       </StyledWrapper>
     </section>
   );
--- src/components/projects/Projects.style.js
+++ src/components/projects/Projects.style.js
@@ -6,3 +6,10 @@ export const StyledArticle = styled.article`
   border-radius: 5px;
   box-shadow: rgba(90, 90, 90, 0.1) 1px 7px 29px 0px;
 `;
+
+export const StyledProjectGrid = styled.div`
+  @media screen and (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
